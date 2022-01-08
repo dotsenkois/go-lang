f3ae71c5bf0626942bd4c0011a7d5c49e4d30508
dotsenkois <dotsenkois@yandex.ru>
2022-01-08 17:15:26 +0700

    http://golang-book.ru./chapter-06-arrays-slices-maps.html

--- src/golang-book/chapter2/main.go
+++ src/golang-book/chapter2/main.go
@@ -2,11 +2,19 @@ package main
 
 import "fmt"
 
-// this is a comment
-
 func main() {
-
-	var name string
-	fmt.Scanf("%s\n", &name)
-	fmt.Println("Hello, my name is %s\n", name)
+	x := []int{
+		48, 96, 86, 68,
+		57, 82, 63, 70,
+		37, 34, 83, 27,
+		19, 97, 9, 17,
+	}
+	min := 100000
+	for _, value := range x {
+		if value < min {
+			fmt.Println(value)
+			min = value
+		}
+	}
+	fmt.Println(min)
 }
