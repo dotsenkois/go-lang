569ca7cc62a7cc2c0219f98f7702a5c10c032efb
dotsenkois <dotsenkois@yandex.ru>
2022-01-09 12:54:14 +0700

    http://golang-book.ru./chapter-07-functions.html (6)

--- src/golang-book/chapter2/main.go
+++ src/golang-book/chapter2/main.go
@@ -1,18 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func makeOddGenerator() func() uint {
-	i := uint(1)
-	return func() (ret uint) {
-		ret = i
-		i += 2
-		return
+func fibonacchi(x int) int {
+	if x < 2 {
+		return 1
 	}
+	return fibonacchi(x-2) + fibonacchi(x-1)
+}
+
+func printFib(x int) {
+	for i := 1; i < x; i++ {
+		fmt.Print(fibonacchi(i), " ")
+	}
+
 }
 func main() {
-	nextEven := makeOddGenerator()
-	fmt.Println(nextEven())
-	fmt.Println(nextEven())
-	fmt.Println(nextEven())
+	printFib(10)
 }
